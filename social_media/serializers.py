645fb73a8630bbfcb1615a2daa25ab00de775f69
uzairmin <uzair.am10@gmail.com>
2024-07-19 20:01:09 +0500

    fields added

--- social_media/serializers.py
+++ social_media/serializers.py
@@ -6,5 +6,5 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = ["name", "category", "founder", "description"]
         depth = 1
